535d714918ab0b64d053a9e1715276d88702e9bc
Aidan Lim <ortivo.sol@gmail.com>
2019-10-28 00:50:30 -0700

    update getting map information

--- app/src/components/function/match/index.jsx
+++ app/src/components/function/match/index.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 import './index.css';
 
 const Matching = () => {
+	
+	// navigator.geolocation.getCurrentPosition((position) => {
+	// 	console.log(position.coords.latitude + ',' + position.coords.longitude);
+	// });
+
 	return (
 		<div className='matching'>
 			Matching
