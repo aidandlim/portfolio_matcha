db97856417cea62b42f7c639f99f307afcedb465
Aidan Lim <ortivo.sol@gmail.com>
2019-10-27 20:54:02 -0700

    update navingation bar

--- app/src/components/template/footer/index.jsx
+++ app/src/components/template/footer/index.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { ui_nav } from '../../../actions';
+
 import { FiAtSign, FiMap, FiMessageCircle, FiHeart } from "react-icons/fi";
 import './index.css';
 
 const Footer = () => {
+	const ui = useSelector(state => state.ui);
+	const dispatch = useDispatch();
+
 	return (
 		<div className='footer'>
 			<div className='footer-container'>
-				<div className='footer-icon-container'>
-					<FiAtSign className='footer-icon' />
-					<div className='footer-title'>MATCH</div>
+				<div className='footer-icon-container' onClick={ () => dispatch(ui_nav(1)) }>
+					<FiAtSign className={ui.nav === 1 ? 'footer-icon-active' : 'footer-icon'} />
+					<div className={ui.nav === 1 ? 'footer-title-active' : 'footer-title'}>MATCH</div>
 				</div>
-				<div className='footer-icon-container'>
-					<FiMap className='footer-icon' />
-					<div className='footer-title'>FIND</div>
+				<div className='footer-icon-container' onClick={ () => dispatch(ui_nav(2)) }>
+					<FiMap className={ui.nav === 2 ? 'footer-icon-active' : 'footer-icon'} />
+					<div className={ui.nav === 2 ? 'footer-title-active' : 'footer-title'}>FIND</div>
 				</div>
-				<div className='footer-icon-container'>
-					<FiMessageCircle className='footer-icon' />
-					<div className='footer-title'>CHAT</div>
+				<div className='footer-icon-container' onClick={ () => dispatch(ui_nav(3)) }>
+					<FiMessageCircle className={ui.nav === 3 ? 'footer-icon-active' : 'footer-icon'} />
+					<div className={ui.nav === 3 ? 'footer-title-active' : 'footer-title'}>CHAT</div>
 				</div>
-				<div className='footer-icon-container'>
-					<FiHeart className='footer-icon' />
-					<div className='footer-title'>NOTIFY</div>
+				<div className='footer-icon-container' onClick={ () => dispatch(ui_nav(4)) }>
+					<FiHeart className={ui.nav === 4 ? 'footer-icon-active' : 'footer-icon'} />
+					<div className={ui.nav === 4 ? 'footer-title-active' : 'footer-title'}>NOTIFY</div>
 				</div>
 			</div>
 		</div>
--- app/src/components/template/header/index.jsx
+++ app/src/components/template/header/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import { useDispatch } from 'react-redux';
+import { ui_nav } from '../../../actions';
+
 import './index.css';
 
 const Header = () => {
+	const dispatch = useDispatch();
+
 	return (
 		<div className='header'>
 			<div className='header-title'>M@TCH@</div>
-			<div className='header-user'></div>
+			<div className='header-user' onClick={ () => dispatch(ui_nav(0)) }></div>
 		</div>
 	);
 }
