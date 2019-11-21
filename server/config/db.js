930f6c6d5a367c035a4e2e1c76da2b3ac7d224ff
Kil-soon Kim <kilkim@e1z2r3p34.42.us.org>
2019-11-21 15:56:06 -0800

    db_updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,29 @@
 const mysql = require('mysql');
 
-exports.module = mysql.createConnection({
+const conn = mysql.createConnection({
     host: '54.215.128.212',
     port: '3306',
     user: 'matcha',
     password: 'matcha',
     database: 'com_aidandlim_matcha'
-}).connect();
+})
+
+conn.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection was closed.')
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+            console.error('Database has too many connections.')
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('Database connection was refused.')
+        }
+    }
+    if (connection) {
+        connection.release();
+    }
+    return ;
+})
+
+module.exports = conn
