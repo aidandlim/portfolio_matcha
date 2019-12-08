bb82052e82c93b088b54b3317df5a2a1b8622d12
aidandlim <aidandlim@gmail.com>
2019-12-07 18:49:12 -0800

    update modules

--- app/src/components/unit/profile/theme/index.jsx
+++ app/src/components/unit/profile/theme/index.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
 
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { ui_color } from '../../../../actions';
 
-import cookie from 'react-cookies'
-
-import { HuePicker } from 'react-color';
+import cookie from 'react-cookies';
 
 import '../index.css';
 
 const Theme = () => {
-	const ui = useSelector(state => state.ui);
 	const dispatch = useDispatch();
 
+	const colors = [
+		'#e74c3c',
+		'#e67e22',
+		'#f1c40f',
+		'#1abc9c',
+		'#2ecc71',
+		'#3498db',
+		'#9b59b6',
+		'#34495e',
+		'#7f8c8d',
+	];
+
 	const _handleColor = (color) => {
-		cookie.save('theme-color', color.hex, { path: '/' });
-		dispatch(ui_color(color.hex));
+		cookie.save('theme-color', color, { path: '/' });
+		dispatch(ui_color(color));
 	}
 
 	return (
@@ -23,12 +32,17 @@ const Theme = () => {
 			<div className='profile-title'>Theme</div>
 			<div className='profile-description'>I currently have 4 windows open up… and I don’t know why.</div>
 			<div className='profile-section'>
-				<HuePicker 
-					className='profile-theme' 
-					width='calc(100% - 1rem)'
-					color={ui.color} 
-					onChange={_handleColor}
-				/>
+				{colors.map((color, index) =>
+					<div className='profile-theme-example' key={index} onClick={ () => _handleColor(color) }>
+						<div className='profile-theme-exmaple-nav' style={{ backgroundColor: color}}></div>
+						<div className='profile-theme-exmaple-body' style={{ backgroundColor: '#ecf0f1'}}>
+							<div className='profile-theme-exmaple-container'>
+								<div className='profile-theme-example-container-header' style={{ backgroundColor: color}}></div>
+							</div>
+						</div>
+						<div className='profile-theme-exmaple-sidebar' style={{ backgroundColor: color}}></div>
+					</div>
+				)}
 			</div>
 		</div>
 	);
