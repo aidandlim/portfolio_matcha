2456a88513ae148e7f2f9fa4ff8bd34863203b6d
Luke K <dyobi@DYOBI.local>
2019-12-09 01:07:33 -0800

    overviews

--- server/route/overviews.js
+++ server/route/overviews.js
@@ -1,22 +1,11 @@
 const conn = require('../config/db');
 
 module.exports.select = (req, res) => {
-    const sql_count_appears = 'SELECT COUNT(*) as appears FROM appears WHERE time > SUBDATE(NOW(), INTERVAL 7 DAY) GROUP BY DAY(time) AND `to` = (SELECT id FROM users WHERE email = ?)';
-    const sql_count_visits = 'SELECT COUNT(*) as visits FROM visits WHERE time > SUBDATE(NOW(), INTERVAL 7 DAY) GROUP BY DAY(time) AND `to` = (SELECT id FROM users WHERE email = ?)';
-    const sql_count_likes = 'SELECT COUNT(*) as likes FROM likes WHERE time > SUBDATE(NOW(), INTERVAL 7 DAY) GROUP BY DAY(time) AND `to` = (SELECT id FROM users WHERE email = ?)';
-    const sql_count_unlikes = 'SELECT COUNT(*) as unlikes FROM unlikes WHERE time > SUBDATE(NOW(), INTERVAL 7 DAY) GROUP BY DAY(time) AND `to` = (SELECT id FROM users WHERE email = ?)';
+	const sql = 'SELECT \'appears\' AS type, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 6 DAY) THEN 1 END) as date1, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 5 DAY) THEN 1 END) as date2, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 4 DAY) THEN 1 END) as date3, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 3 DAY) THEN 1 END) as date4, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 2 DAY) THEN 1 END) as date5, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 1 DAY) THEN 1 END) as date6, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 0 DAY) THEN 1 END) as date7 FROM appears WHERE `to` = (SELECT id FROM users WHERE email = ?) UNION SELECT \'visits\' AS type, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 6 DAY) THEN 1 END) as date1, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 5 DAY) THEN 1 END) as date2, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 4 DAY) THEN 1 END) as date3, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 3 DAY) THEN 1 END) as date4, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 2 DAY) THEN 1 END) as date5, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 1 DAY) THEN 1 END) as date6, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 0 DAY) THEN 1 END) as date7 FROM visits WHERE `to` = (SELECT id FROM users WHERE email = ?) UNION SELECT \'likes\' AS type, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 6 DAY) THEN 1 END) as date1, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 5 DAY) THEN 1 END) as date2, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 4 DAY) THEN 1 END) as date3, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 3 DAY) THEN 1 END) as date4, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 2 DAY) THEN 1 END) as date5, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 1 DAY) THEN 1 END) as date6, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 0 DAY) THEN 1 END) as date7 FROM likes WHERE `to` = (SELECT id FROM users WHERE email = ?) UNION SELECT \'unlikes\' AS type, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 6 DAY) THEN 1 END) as date1, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 5 DAY) THEN 1 END) as date2, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 4 DAY) THEN 1 END) as date3, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 3 DAY) THEN 1 END) as date4, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 2 DAY) THEN 1 END) as date5, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 1 DAY) THEN 1 END) as date6, count(CASE WHEN DATE(time) = (CURDATE() - INTERVAL 0 DAY) THEN 1 END) as date7 FROM unlikes WHERE `to` = (SELECT id FROM users WHERE email = ?)';
 
-    // const email = req.session.user;
-    // const temp = {
-    //     appears: [],
-    //     visits: [],
-    //     likes: [],
-    //     unlikes: []
-    // };
+    const email = req.session.user;
 
-    const email = 'aidandlim@gmail.com';
-
-    conn.query(sql_count_appears, [email], (err, results) => {
+    conn.query(sql_count_appears, [email, email, email, email], (err, results) => {
         if (err) {
             console.log(err);
         } else {
@@ -24,65 +13,5 @@ module.exports.select = (req, res) => {
             console.log(results);
         }
     })
-
-
 }
 
-/*
-
-SELECT 
-	'appears' AS type,
-	count(CASE WHEN DATE(time) = '2019-12-03' THEN 1 END) as date1,
-	count(CASE WHEN DATE(time) = '2019-12-04' THEN 1 END) as date2,
-	count(CASE WHEN DATE(time) = '2019-12-05' THEN 1 END) as date3,
-	count(CASE WHEN DATE(time) = '2019-12-06' THEN 1 END) as date4,
-	count(CASE WHEN DATE(time) = '2019-12-07' THEN 1 END) as date5,
-	count(CASE WHEN DATE(time) = '2019-12-08' THEN 1 END) as date6,
-	count(CASE WHEN DATE(time) = '2019-12-09' THEN 1 END) as date7
-FROM appears 
-WHERE `to` = 35
-
-UNION
-
-SELECT 
-	'visits' AS type,
-	count(CASE WHEN DATE(time) = '2019-12-03' THEN 1 END) as date1,
-	count(CASE WHEN DATE(time) = '2019-12-04' THEN 1 END) as date2,
-	count(CASE WHEN DATE(time) = '2019-12-05' THEN 1 END) as date3,
-	count(CASE WHEN DATE(time) = '2019-12-06' THEN 1 END) as date4,
-	count(CASE WHEN DATE(time) = '2019-12-07' THEN 1 END) as date5,
-	count(CASE WHEN DATE(time) = '2019-12-08' THEN 1 END) as date6,
-	count(CASE WHEN DATE(time) = '2019-12-09' THEN 1 END) as date7
-FROM visits 
-WHERE `to` = 35
-
-UNION
-
-SELECT 
-	'likes' AS type,
-	count(CASE WHEN DATE(time) = '2019-12-03' THEN 1 END) as date1,
-	count(CASE WHEN DATE(time) = '2019-12-04' THEN 1 END) as date2,
-	count(CASE WHEN DATE(time) = '2019-12-05' THEN 1 END) as date3,
-	count(CASE WHEN DATE(time) = '2019-12-06' THEN 1 END) as date4,
-	count(CASE WHEN DATE(time) = '2019-12-07' THEN 1 END) as date5,
-	count(CASE WHEN DATE(time) = '2019-12-08' THEN 1 END) as date6,
-	count(CASE WHEN DATE(time) = '2019-12-09' THEN 1 END) as date7
-FROM likes 
-WHERE `to` = 35
-
-UNION
-
-SELECT 
-	'unlikes' AS type,
-	count(CASE WHEN DATE(time) = '2019-12-03' THEN 1 END) as date1,
-	count(CASE WHEN DATE(time) = '2019-12-04' THEN 1 END) as date2,
-	count(CASE WHEN DATE(time) = '2019-12-05' THEN 1 END) as date3,
-	count(CASE WHEN DATE(time) = '2019-12-06' THEN 1 END) as date4,
-	count(CASE WHEN DATE(time) = '2019-12-07' THEN 1 END) as date5,
-	count(CASE WHEN DATE(time) = '2019-12-08' THEN 1 END) as date6,
-	count(CASE WHEN DATE(time) = '2019-12-09' THEN 1 END) as date7
-FROM unlikes 
-WHERE `to` = 35
-
-
-*/
