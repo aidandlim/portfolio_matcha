9928e8e28d4aa9b5573e5918f7cc3a4e315ab7a0
aidandlim <aidandlim@gmail.com>
2019-12-09 22:43:58 -0800

    update modules

--- app/src/components/unit/profile/close/index.jsx
+++ app/src/components/unit/profile/close/index.jsx
@@ -8,9 +8,7 @@ const Close = () => {
 			<div className='profile-title'>Close Account</div>
 			<div className='profile-description'>Sometimes it is better to just walk away from things and go back to them later when you’re in a better frame of mind.</div>
 			<div className='profile-section'>
-				<form onSubmit={_handleForm}>
-					<input type='submit' className='profile-submit' value='CLOSE MY ACCOUNT' />
-				</form>
+				<input type='submit' className='profile-submit' value='CLOSE MY ACCOUNT' />
 			</div>
 		</div>
 	);
